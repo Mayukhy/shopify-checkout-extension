8afe72d95756f2458db0d0fbb7d25d6546a3f2ef
Mayukh Das <dasmayukh2019@gmail.com>
2025-10-02 14:23:36 +0530

    FEATURE: add product variant settings to checkout-upsell extension

--- extensions/checkout-upsell/src/Checkout.tsx
+++ extensions/checkout-upsell/src/Checkout.tsx
@@ -2,40 +2,44 @@ import {
   reactExtension,
   Banner,
   BlockStack,
-  Checkbox,
-  Text,
   useApi,
-  useApplyAttributeChange,
   useInstructions,
   useTranslate,
+  useSettings,
 } from "@shopify/ui-extensions-react/checkout";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import ProductCard from "./components/ProductCard";
 
 // 1. Choose an extension target
-export default reactExtension("purchase.checkout.cart-line-list.render-after", () => (
-  <Extension />
-));
+export default reactExtension(
+  "purchase.checkout.cart-line-list.render-after",
+  () => <Extension />
+);
 
 function Extension() {
   const translate = useTranslate();
   const { extension, query } = useApi();
   const instructions = useInstructions();
+  const { product_variant_1, product_variant_2, product_variant_3 } =
+    useSettings();
 
-  const variantIdArr = [
-    "gid://shopify/ProductVariant/40954715537443",
-    "gid://shopify/ProductVariant/40954713899043",
-    "gid://shopify/ProductVariant/40954714259491",
-  ];
+  const variantIdArr = useMemo(() => {
+    return [product_variant_1, product_variant_2, product_variant_3].filter(
+      Boolean
+    ) as string[]; // Filter out undefined values
+  }, [product_variant_1, product_variant_2, product_variant_3]);
+
   const [productVariants, setProductVariants] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    getProductVariantsData();
+    if (variantIdArr.length > 0) {
+      getProductVariantsData();
+    }
   }, []);
 
   //graphql query to get the product data of the variant ids
-  const getProductVariantsData = async() => {
+  const getProductVariantsData = async () => {
     const variants = [];
     for (let i = 0; i < variantIdArr.length; i++) {
       const result = await query(`{
@@ -56,13 +60,11 @@ function Extension() {
               }
             }
           }
-        }`
-      );
+        }`);
       variants.push(result);
     }
     setProductVariants(variants);
   };
-
 
   // 2. Check instructions for feature availability, see https://shopify.dev/docs/api/checkout-ui-extensions/apis/cart-instructions for details
   if (!instructions.attributes.canUpdateAttributes) {
@@ -78,18 +80,17 @@ function Extension() {
   // 3. Render a UI
   return (
     <BlockStack>
-      {
-        productVariants.map(variant => (
+      {productVariants.length > 0 &&
+        productVariants.map((variant) => (
           <ProductCard
-          type="radio"
-          key={variant.data.node.id}
-          node={variant.data.node}
-          productVariants={productVariants}
-          loading={loading}
-          setLoading={setLoading}
+            type="radio"
+            key={variant.data.node.id}
+            node={variant.data.node}
+            productVariants={productVariants}
+            loading={loading}
+            setLoading={setLoading}
           />
-        ))
-      }
+        ))}
     </BlockStack>
   );
 }
